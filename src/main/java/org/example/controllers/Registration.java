3b857f7d331eeb4bea25ee207ecb91f82a58d39f
HosakooD <hosakoo2020@gmail.com>
2023-01-08 22:13:05 +0200

    Initial commit

---
+++ src/main/java/org/example/controllers/Registration.java
@@ -0,0 +1,39 @@
+package org.example.controllers;
+
+import org.example.domain.Role;
+import org.example.domain.User;
+import org.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class Registration {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/org/example/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.repositories;
+
+import org.example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
