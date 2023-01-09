38bc3edc561ec0124424ae18d5bb0bf2d30a03f3
HosakooD <hosakoo2020@gmail.com>
2023-01-09 19:18:35 +0200

    Updated security/added images

--- src/main/java/org/example/config/MvcConfig.java
+++ src/main/java/org/example/config/MvcConfig.java
@@ -1,10 +1,12 @@
 package org.example.config;
 
 import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
@@ -14,8 +16,20 @@ import java.io.IOException;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:/" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 
     @Bean
--- src/main/java/org/example/controllers/MainController.java
+++ src/main/java/org/example/controllers/MainController.java
@@ -1,22 +1,31 @@
 package org.example.controllers;
 
+import org.apache.commons.io.IOUtils;
 import org.example.domain.Message;
 import org.example.domain.User;
 import org.example.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -43,9 +52,21 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model
-    ) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file")MultipartFile file
+            ) throws IOException {
         Message message = new Message(text, tag, user);
+        if(file != null && !file.getOriginalFilename().isEmpty()){
+            File upload = new File(uploadPath);
+            if(!upload.exists()){
+                upload.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+            message.setFilename(resultFileName);
+        }
 
         messageRepository.save(message);
 
@@ -54,5 +75,12 @@ public class MainController {
         model.put("messages", messages);
 
         return "main";
+    }
+
+    @GetMapping(value = "/img/{imageUrl}")
+    public @ResponseBody byte[] image(@PathVariable String imageUrl) throws IOException {
+        String url = "/D:/MyApps/Tweetie/upload/" + imageUrl;
+        InputStream in = new FileInputStream(url);
+        return IOUtils.toByteArray(in);
     }
 }
